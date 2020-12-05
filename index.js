4498e34dd71addfca18d1eff4d94bd3e93cb9f28
mollymoog <mmkover@gmail.com>
2020-12-05 16:13:52 -0500

    commit tree 36e18bf2b29423c8a2aee11f9744d3ab8d3bf6e9

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+
+function map(array, func) {
+    let result = [];
+    for (const el of array) {
+        result.push(func(el));
+    }
+    return result;
+}
+
+function reduce(array, func, startValue) {
+    let r = (!!startValue) ? startValue : array[0]
+    let i = (!!startValue) ? 0 : 1
+    for (; i < array.length; i++) {
+        r = func(array[i], r);
+    }
+    return r;
+}
